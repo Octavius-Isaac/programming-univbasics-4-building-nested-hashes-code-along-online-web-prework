887570cd0363519326dea01e6ac4bfff35f7a51d
Solomon Octavius Isaac <octavius.isaac@gmail.com>
2019-10-11 13:46:54 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -21,8 +21,8 @@ def bonus
   }
 
   epic_tragedy [
-   [[:montague][:hero][:status] = "dead"], [[:capulet][:heroine][:status] = "dead"
-    ]]
+   [[:montague][:hero][:status] = "dead"], [[:capulet][:heroine][:status] = "dead"]
+    ]
 
   
 
